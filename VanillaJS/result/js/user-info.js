634e53dd826c3c019e348070eb8c02f67c3f448d
jian-level-is <myunggeun22@gmail.com>
2021-07-12 14:35:35 +0900

    Update user-info.js

--- VanillaJS/result/js/user-info.js
+++ VanillaJS/result/js/user-info.js
@@ -3,15 +3,18 @@ const loginLabel   = document.querySelector(".login-form label");
 const loginInput   = document.querySelector("#login-id");
 const loginWelcome = document.querySelector(".welcome-text"); 
 
-const handleUserId = (event) =>{    event.preventDefault();
-    //Step1     save user Id to localStorage.
-    const userId = loginInput.value;
-    localStorage.setItem("id", JSON.stringify(userId));
-
-    //Step2     Hide login Form and Show user The Welcome Text.
+const sayHello = ()=> {
     loginForm.classList.add("hide");
-    // getStorageId === ${userId}
     const getStorageId = JSON.parse(localStorage.getItem("id"));
     loginWelcome.innerHTML = `${getStorageId}様、こんばんは！`;
 }
+
+const handleUserId = (event) =>{    
+    event.preventDefault();    
+    const userId = loginInput.value;
+    localStorage.setItem("id", JSON.stringify(userId));
+    sayHello();
+}
+
+if(localStorage.getItem("id")){     sayHello()     };
 loginLabel.addEventListener("click", handleUserId);
